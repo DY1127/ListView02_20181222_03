a450de0322360dfe9a07d991884d3718fdf1f3b2
yorudaydream <yorudaydream@gmail.com>
2018-12-22 13:56:27 +0900

    액티비키 클레스에서 가게목록을 담을 Array list 생성

--- app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
+++ app/src/main/java/kr/tjeit/listview02_20181222_03/MainActivity.java
@@ -2,8 +2,17 @@ package kr.tjeit.listview02_20181222_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.tjeit.listview02_20181222_03.datas.Store;
 
 public class MainActivity extends BaseActivity {
+
+    ListView storeListView;
+    List<Store> storeList = new ArrayList<Store>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +36,6 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+        storeListView = findViewById(R.id.storeListView);
     }
 }
